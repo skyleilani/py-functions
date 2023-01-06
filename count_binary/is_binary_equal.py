174da4e4502b4e1b281f54aee6a78064c9870278
sky leilani <45158875+skyleilani@users.noreply.github.com>
2023-01-06 10:43:43 -0800

    Update is_binary_equal.py

--- count_binary/is_binary_equal.py
+++ count_binary/is_binary_equal.py
@@ -1,4 +1,9 @@
 # is_binary_equal will return true if there are an equal amount of 1's to 0's, and false if there is an unequal amount
-
 def is_binary_equal(binary):
   return binary.count('1')  == binary.count('0')
+
+# tests 
+print(is_binary_equal('100101') # true 
+print(is_binary_equal('1010101') # false 
+print(is_binary_equal('010101') # true 
+print(is_binary_equal('110101') # false
