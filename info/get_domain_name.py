e22d18c7cc417eb00f2a16a1048451f1a9f8c765
sky leilani <45158875+skyleilani@users.noreply.github.com>
2023-01-06 12:20:57 -0800

    Update get_domain_name.py

--- info/get_domain_name.py
+++ info/get_domain_name.py
@@ -6,14 +6,16 @@ def get_domain_name(url):
     print('Error: Input must be a string')
     return
   
-  # split the URL on '/' 
-  split_url = url.split('/')
-  
-  # get second element of split URL(domain name)
-  domain_name = split_url[2]
-  
-  # split domain name on '.' 
-  split_domain_name = domain_name.split('.')
+  // split url on '/' or '.'
+  split_url = url.split('/') if '/' in url else url.split('.')
+  if len(split_url) > 2: 
+    # get second element of split URL(domain name)
+    domain_name = split_url[2]
+    # split domain name on '.' 
+    split_domain_name = domain_name.split('.')
+  else: 
+    domain_name=''
+    return
   
   # return first element of split domain name (domain name without the top level domain)
   return split_domain_name[0]
